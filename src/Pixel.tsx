5abac0732841500a11804dc98ec5317297a03272
OwnKng <owen.king@outlook.com>
2021-12-29 23:27:52 +0000

    Package fix

--- src/Pixel.tsx
+++
@@ -1,8 +0,0 @@
-import { Instance } from "@react-three/drei"
-import { useFrame } from "@react-three/fiber"
-import { useRef } from "react"
-import { Mesh } from "three"
-
-const Pixel = () => <Instance />
-
-export default Pixel
