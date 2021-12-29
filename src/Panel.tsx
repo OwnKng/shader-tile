f533f6238d4faa1d28760c8f3f4ab15ae7e40bbb
OwnKng <owen.king@outlook.com>
2021-12-29 23:11:49 +0000

    Clean up

--- src/Panel.tsx
+++ src/Panel.tsx
@@ -1,6 +1,5 @@
 // @ts-nocheck
-import { useLayoutEffect, useMemo, useRef } from "react"
-import * as THREE from "three"
+import { useMemo, useRef } from "react"
 import Material from "./Material"
 import { useTexture } from "@react-three/drei"
 
@@ -12,6 +11,7 @@ const Panel = () => {
   const height = texture.image.height
   const num = width * height
 
+  //_ vertices and index for the square shape
   const vertices = useMemo(
     () =>
       new Float32Array([
@@ -22,10 +22,10 @@ const Panel = () => {
 
   const index = useMemo(() => new Uint16Array([0, 2, 1, 2, 3, 1]), [])
 
-  const { offsets, indices, colors } = useMemo(() => {
+  //_ positions, indexes and UV coords for each pixel / shape
+  const { offsets, indices } = useMemo(() => {
     const offsets = new Float32Array(num * 3)
     const indices = new Uint16Array(num)
-    const colors = new Float32Array(num * 3)
 
     for (let i = 0; i < num; i++) {
       offsets[i * 3 + 0] = i % width
@@ -33,29 +33,15 @@ const Panel = () => {
       offsets[i * 3 + 2] = 0
 
       indices[i] = i
-
-      const tempColor =
-        Math.random() > 0.5
-          ? new THREE.Color("hotpink")
-          : new THREE.Color("blue")
-
-      colors[i * 3 + 0] = tempColor.r
-      colors[i * 3 + 1] = tempColor.g
-      colors[i * 3 + 2] = tempColor.b
     }
 
-    return { offsets, indices, colors }
+    return { offsets, indices }
   }, [num, width])
 
   const uvs = useMemo(
     () => new Float32Array([0, 0, 0, 1.0, 1.0, 0, 1.0, 1.0]),
     []
   )
-
-  useLayoutEffect(() => {
-    ref.current.setMatrixAt(0, new THREE.Matrix4())
-    ref.current.instanceMatrix.needsUpdate = true
-  })
 
   return (
     <instancedMesh
@@ -82,10 +68,6 @@ const Panel = () => {
         <instancedBufferAttribute
           attachObject={["attributes", "pindex"]}
           args={[indices, 1]}
-        />
-        <instancedBufferAttribute
-          attachObject={["attributes", "pColor"]}
-          args={[colors, 3]}
         />
       </bufferGeometry>
       <Material texture={texture} />
