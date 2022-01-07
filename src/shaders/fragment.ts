e38da52a86d384e0795e7033618aba9243ab3f42
OwnKng <owen.king@outlook.com>
2022-01-07 18:25:59 +0000

    Finished effect

--- src/shaders/fragment.ts
+++ src/shaders/fragment.ts
@@ -15,16 +15,15 @@ export const fragmentShader = /* glsl */ `
 	    return fract(sin(n) * 43758.5453123);
     }
 
-
-
     void main() {
-        float time = sin(vTime * 0.5) * 0.5 + 0.5;
-        vec2 grid = vec2(particleuv.x * 6.0, particleuv.y * 8.0);
+        float time = sin(vTime * 0.8) * 0.5 + 0.5;
+        vec2 grid = particleuv * 10.0;
         vec2 ipos = ceil(grid); 
 
-        float checks = cnoise(vec3(ipos * 10.0 + time, vStrength * 0.5));
-
-        vec3 color = hsl2rgb(0.5, 0.5, vStrength);
+        float checks = cnoise(vec3(ipos * 10.0 + time, 0.1));
+        checks *= 5.0;
+    
+        vec3 color = hsl2rgb(0.9 + checks * 0.05, 0.8, vStrength);
 
         gl_FragColor = vec4(color, checks);
     }
